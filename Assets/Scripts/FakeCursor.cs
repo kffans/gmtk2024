93babeaba88b93e3a88e4bcb47c1e2ee4085dd2c
kffans <kubakwan@gmail.com>
2024-08-19 23:03:34 +0200

    trying to make better cursor

--- Assets/Scripts/FakeCursor.cs
+++ Assets/Scripts/FakeCursor.cs
@@ -5,12 +5,21 @@ public class FakeCursor : MonoBehaviour
     public static Transform instance;
 	public GameObject objParent;
 	public GameObject fakeCursor;
+	public Camera cam;
 	
 	void Awake(){
         instance = Instantiate(fakeCursor, objParent.transform).transform;
 	}
 	
     void Update(){
-        instance.position = Input.mousePosition;
+		//Debug.Log(cam.ScreenToWorldPoint(Input.mousePosition));
+		Vector3 point = new Vector3();
+        Vector2 mousePos = new Vector2();
+		mousePos.x = Input.mousePosition.x;
+        mousePos.y =  2*Input.mousePosition.y - cam.pixelHeight;
+
+        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+		Debug.Log(point);
+        instance.position = point;
     }
 }
