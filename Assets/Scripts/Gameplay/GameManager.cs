353913231811b68a5c4e1d93d5ad852b98ff6239
kffans <kubakwan@gmail.com>
2024-08-25 17:14:07 +0200

    forgot to update: GameManager cameras, changed scenefight

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -37,6 +37,7 @@ public class GameManager : MonoBehaviour
 	private int swordLevel = 0;
 	public GameObject[] swordBlades;
 	public Transform currentSwordBlade;
+	public AnimationCurve swordCurve;
 	
 	//arms
 	public RawImage arms;
@@ -155,15 +156,46 @@ public class GameManager : MonoBehaviour
 					if(enemyData[stage].swordLevelUp>0){ //level up miecza
 						swordLevel += enemyData[stage].swordLevelUp;
 					}
+					Camera cam = cameraRect.GetComponent<Camera>();
+					
+					//destroy enemy
+					LeanTween.scaleY(enemy.gameObject, 0f, 0f);
+					//make new one in designated place
+					Transform enemyTemp = Instantiate(enemyData[stage].prefab, enemyParent.transform).transform;
+					//set position of enemy
+					enemyTemp.position = new Vector3(10-8*stage,enemyTemp.position.y,0);
+					//zoom the camera
+					cam.orthographicSize = 300+20*swordLevel;
+					//leantween sword & arms rotation, change arms midway
+					Transform armsTr = arms.GetComponent<Transform>();
+					Sword.RotateZTo(swordHandle.transform, Sword.swingRotationMax);
+					Sword.RotateZTo(armsTr,1);
+					
+					LeanTween.rotateZ(swordHandle, 130f, 0.4f).setEase(swordCurve).setDelay(0.2f);
+					LeanTween.rotateZ(armsTr.gameObject, 119f, 0.4f).setEase(swordCurve).setDelay(0.2f);
+					//play sound
+					if(stage<6)
+						AudioManager.instance.PlaySFX("slay-soft");
+					else
+						AudioManager.instance.PlaySFX("slay-hard");
+					//death anim with scale y
+					LeanTween.scaleY(enemyTemp.gameObject, 0f, 0.6f).setDelay(1.6f);
+					//shake camera
+					//leantween camera zoom out,
+					
+					
 					
 					if(enemyData.Length == stage+1){
 						timeCount=0f; currentPhase = Phase.Win; doneOnceEnemyDeath = false; goto Beginning;
 					}
 					else stage++;
 				}
+				cameraRect.position = new Vector3(currentSwordBlade.position.x, currentSwordBlade.position.y+50*swordLevel, -10);
+				
 				
 				if(timeCount < durationEnemyDeath) { timeCount += Time.deltaTime; }
 				else { 
+					cameraRect.GetComponent<Camera>().orthographicSize = 540;
 					timeCount=0f; currentPhase = Phase.Intro;
 					if(enemyData[stage-1].swordLevelUp>0) currentPhase = Phase.SwordGrow;
 					doneOnceEnemyDeath = false; goto Beginning;
@@ -197,6 +229,7 @@ public class GameManager : MonoBehaviour
 					Debug.Log("aa");
 					//camera to different place to show blade
 					//cameraRect.position = new Vector3(0f,0f,-10f); //reset camera position
+					//"The blade has visibly grown in size...!"
 				}
 				
 				if(timeCount < durationSwordGrow) { timeCount += Time.deltaTime; }
